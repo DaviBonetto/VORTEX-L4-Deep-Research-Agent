f4b6c1a4e0e9ec3e0b25d050d2cbd74cae1982c4
Davi Bonetto <davi.bonetto100@gmail.com>
2026-01-22 08:59:31 -0300

    feat(state): add analysis_content field to AgentState

--- src/state/graph.py
+++ src/state/graph.py
@@ -3,5 +3,6 @@ from typing import TypedDict, List
 class AgentState(TypedDict):
     task: str
     research_data: List[str]
+    analysis_content: str
     report_content: str
     revision_count: int
